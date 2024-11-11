e763678a562e83afc10896e82eca2a73cdee6c2d
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-11 17:04:16 +0100

    edit main layout and created home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,14 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+        <div class="col-md-10">
+            <div class="card mb-4">
+                <div class="card-header">{{ __('Benvenuto nel nostro Progetto') }}</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <p>Benvenuto nella piattaforma Social-Media!</p>
+                    <p>Qui puoi creare, visualizzare e interagire con i post. Registrati o accedi per iniziare a esplorare i contenuti e partecipare alla community.</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center py-4">
+    <div class="container">
+        <p>&copy; {{ date('Y') }} {{ config('app.name', 'Laravel') }}. All rights reserved.</p>
+    </div>
+</footer>
