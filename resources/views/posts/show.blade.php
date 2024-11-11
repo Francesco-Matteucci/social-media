c1be8cc1cbf6f54fab57586d637886cb9254a2a2
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-11 17:30:11 +0100

    update show

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fw-bold text-center fst-italic">
+                    Post Show
+                </h1>
+            </div>
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Content</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                {{ $post['id'] }}
+                            </td>
+                            <td>
+                                {{ $post['content'] }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endsection
