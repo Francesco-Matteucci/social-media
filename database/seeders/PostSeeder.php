8066a1e0456dac8f595b56a8779ba3837f20c306
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-11 17:08:56 +0100

    added PostSeeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+            $post->used_id = $faker->numberBetween(1, 10);
+            $post->category_id = $faker->numberBetween(1, 5);
+            $post->content = $faker->text(500);
+            $post->save();
+        }
+    }
+}
