b659a6036974115f4cd5a083145d95b83f924951
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-11 17:28:51 +0100

    feat: add bootstrap classes to form inputs group

--- resources/views/layouts/createOrEdit.blade.php
+++ resources/views/layouts/createOrEdit.blade.php
@@ -10,26 +10,29 @@
 
         @yield('form-method','')
         <!-- user id -->
-        <div>
-            <label for="used-id">User</label>
-            <input type="text" name="used_id" id="used-id" value="{{old('used_id',$post->used_id)}}">
+        <div class="mb-2">
+            <label class="form-label" for="used-id">User</label>
+            <input class="form-control" type="text" name="used_id" id="used-id"
+                value="{{old('used_id',$post->used_id)}}">
             @error('used_id')
             <div>{{$message}}</div>
             @enderror
         </div>
         <!-- category_id -->
-        <div>
+        <div class="mb-2">
             <label for="category-id">Post category</label>
-            <input type="text" name="category_id" id="category-id" value="{{old('category_id',$post->category_id)}}">
+            <input class="form-control" type="text" name="category_id" id="category-id"
+                value="{{old('category_id',$post->category_id)}}">
             @error('category_id')
             <div>{{$message}}</div>
             @enderror
         </div>
 
         <!-- content -->
-        <div>
+        <div class="mb-2">
             <label for="content">Content</label>
-            <input type="text" name="content" id="content" value="{{old('content',$post->content)}}">
+            <input class="form-control" type="text" name="content" id="content"
+                value="{{old('content',$post->content)}}">
             @error('content')
             <div>{{$message}}</div>
             @enderror
@@ -38,8 +41,8 @@
         <!-- section for addtional inputs -->
         @yield('additionals-inputs','')
 
-        <div>
-            <button class="btn btn-warning" type="submit">@yield('submit-btn-text', '')</button>
+        <div class="mb-2">
+            <button class="btn btn-success" type="submit">@yield('submit-btn-text', '')</button>
             <button class="btn btn-warning" type="reset">@yield('reset-btn-text', '')</button>
         </div>
 
