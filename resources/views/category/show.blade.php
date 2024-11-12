a89a78f05bd6966577740d29eab1899257ef6dc5
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-12 17:14:07 +0100

    show title

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="fw-bold fst-italic text-decoration-underline">Post Show !</h1>
+                <h1 class="fw-bold fst-italic text-decoration-underline">Post Show !!</h1>
             </div>
         </div>
         <div class="col-12">
