d59914c67f3e53150e14f557e42462e9e30be40a
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-12 14:48:19 +0100

    feat: added fillables in to posts model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'used_id',
+        'category_id',
+        'content',
+    ];
 }
