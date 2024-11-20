6cfa8f8632cc5ad572f586392859fe8add513d36
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-20 16:38:43 +0100

    added api controller, route and index

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json([
+            'success' => true,
+            'result' => $posts,
+        ]);
+    }
+}
